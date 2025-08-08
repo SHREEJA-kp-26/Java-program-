045cb72973eab48a906309f1777f70bc6c544ee4
SHREEJA-kp-26 <kpshreeja04@gmail.com>
2025-08-08 09:22:20 +0530

    Create method_hello.java

---
+++ method_hello.java
@@ -0,0 +1,10 @@
+public class method_hello{
+    String hi(String a){
+        
+       return a;
+    }
+ public static void main(String[] args){
+    method_hello obj = new method_hello();
+    System.out.println(obj.hi("Hello, Java!"));
+ }
+}
